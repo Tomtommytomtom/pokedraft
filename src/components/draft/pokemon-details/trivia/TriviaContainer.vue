971ee4dcbb491fd4e12ad7958aacbbdae14ccd09
Tom <tom.pillokat@googlemail.com>
2020-01-17 16:53:35 +0100

    Add trivia container with placeholder values

---
+++ src/components/draft/pokemon-details/trivia/TriviaContainer.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card
+    class="d-flex flex-column justify-space-between pa-2"
+    style="border: solid 1px rgba(255,255,255,0.2)"
+  >
+    <v-card-title>name</v-card-title>
+    <p>types</p>
+    <v-col class="d-flex">
+      <p>weakness</p>
+      <p>resistance</p>
+    </v-col>
+    <v-card-text
+      style="border: solid 1px rgba(255,255,255,0.2)"
+      fill-height
+      class="pa-2"
+    >
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quae
+      eius obcaecati soluta accusamus harum at excepturi fugit nemo, porro illo
+      quisquam laudantium. Quibusdam fugit non nobis et repudiandae
+      iste.</v-card-text
+    >
+  </v-card>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
