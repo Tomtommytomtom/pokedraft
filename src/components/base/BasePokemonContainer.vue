41349b41ebad57a4789af9b28b7116585ad98884
Tom <tom.pillokat@googlemail.com>
2020-01-18 18:29:16 +0100

    Basic non interactive pokemon container

---
+++ src/components/base/BasePokemonContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card
+    width="124px"
+    style="border-radius: 3px; border: solid rgba(255,255,255,0.1) 1px"
+  >
+    <v-img :src="pokemon.getSprite()" :alt="pokemon.name" height="124px" />
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    pokemon: {
+      type: Object
+    }
+  },
+
+  data() {
+    return {
+      pokemonData: this.pokemon
+    }
+  }
+}
+</script>
