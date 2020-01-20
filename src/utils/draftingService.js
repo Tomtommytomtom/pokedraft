c6cb74c9638a2a05f3c5e3a2d130c21211ad636b
Tom <tom.pillokat@googlemail.com>
2020-01-20 18:28:48 +0100

    Remove commented out code

--- src/utils/draftingService.js
+++ src/utils/draftingService.js
@@ -1,8 +1,6 @@
 import pokeApiService from '@/apis/pokeApiService'
 import Pokemon from './pokemon'
 
-//const LIST_OF_ITEM_CATEGORIES = [5, 7, 12, 13, 15, 17, 44, 46]
-
 const draftSixPokemonAnd = fn => {
   pokeApiService
     .getSixPokemon(generateSixRandomids())
@@ -21,15 +19,6 @@ const generateSixRandomids = () => {
   let ids = [1, 2, 3, 4, 5, 6]
   return ids.map(() => Math.floor(Math.random() * 807 + 1))
 }
-
-// const getRandomItemCategory = () => {
-//   const randomIndex = Math.floor(
-//     Math.random() * LIST_OF_ITEM_CATEGORIES.length + 1
-//   )
-//   return LIST_OF_ITEM_CATEGORIES[randomIndex]
-// }
-
-// const draftItemForId = id => {}
 
 export default {
   draftSixPokemonAnd
