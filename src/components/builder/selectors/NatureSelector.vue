12855b9cc757b7baeb83f7a829e42c636a7ab27b
Tom <tom.pillokat@googlemail.com>
2020-01-21 21:11:44 +0100

    Fill rough nature selector

--- src/components/builder/selectors/NatureSelector.vue
+++ src/components/builder/selectors/NatureSelector.vue
@@ -1,9 +1,38 @@
 <template>
-  <h1>nature-selector</h1>
+  <v-select v-model="selectedNature" :items="natures" @change="emit"></v-select>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    value: {
+      type: String
+    },
+    natures: {
+      type: Array
+    }
+  },
+  data() {
+    return {
+      selectedNature: this.natures[0]
+    }
+  },
+  computed: {
+    natureDisplay: {
+      get() {
+        return this.selectedNature.charAt(0).toUpperCase()
+      },
+      set(newValue) {
+        this.selectedNature = newValue.toLowerCase()
+      }
+    }
+  },
+  methods: {
+    emit() {
+      this.$emit('input', this.selectedNature)
+    }
+  }
+}
 </script>
 
 <style></style>
