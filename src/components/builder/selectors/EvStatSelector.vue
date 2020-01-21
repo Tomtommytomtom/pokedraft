90d3a488e08639f2193cdb96393dcf590629dac3
Tom <tom.pillokat@googlemail.com>
2020-01-21 21:12:27 +0100

    Add first draft of ev selection

--- src/components/builder/selectors/EvStatSelector.vue
+++ src/components/builder/selectors/EvStatSelector.vue
@@ -1,9 +1,54 @@
 <template>
-  <h1>ev-stat-selector</h1>
+  <div>
+    <single-ev-selector
+      v-for="n in 6"
+      :key="n - 1"
+      :index="n - 1"
+      :input="evSelected"
+      :label="labels[n - 1]"
+      :current-cap="capValue"
+    ></single-ev-selector>
+    <p>{{ evs }},{{ sum }},{{ capValue }}</p>
+  </div>
 </template>
 
 <script>
-export default {}
+import SingleEvSelector from './ev-stat-selector/SingleEvSelector'
+export default {
+  components: {
+    SingleEvSelector
+  },
+  props: {
+    labels: {
+      type: Array
+    }
+  },
+  data() {
+    return {
+      evs: [0, 0, 0, 0, 0, 0],
+      max: 510
+    }
+  },
+  computed: {
+    sum() {
+      return this.evs.reduce((curr, acc) => {
+        return curr + acc
+      }, 0)
+    },
+    capValue() {
+      return this.max - this.sum >= 252 ? 252 : this.max - this.sum
+    }
+  },
+  methods: {
+    recalc(n) {
+      console.log('recalcing', this.labels[n])
+      this.$set(this.evs, n, 0)
+    },
+    evSelected(index, value) {
+      console.log(index, value)
+    }
+  }
+}
 </script>
 
 <style></style>
---
+++ src/components/builder/selectors/ev-stat-selector/SingleEvSelector.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="d-flex">
+    <v-col cols="3" class="pa-0 ma-0">
+      <v-text-field
+        v-model="evText"
+        :label="label"
+        dense
+        width="20px"
+        outlined
+        @change="emit"
+      ></v-text-field
+    ></v-col>
+    <v-col class="pa-0 ma-0">
+      <v-slider
+        v-model="evSlider"
+        dense
+        :max="252"
+        :ticks="false"
+        :step="4"
+        min="0"
+        @change="emit"
+      ></v-slider
+    ></v-col>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String
+    },
+    index: {
+      type: Number
+    }
+  },
+  data() {
+    return {
+      evSlider: 0,
+      evText: 0
+    }
+  },
+  methods: {
+    emit() {
+      this.evText = this.evSlider
+      this.$emit('input', this.index, this.evValue)
+    }
+  }
+}
+</script>
+
+<style></style>
