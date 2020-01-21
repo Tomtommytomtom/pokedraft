9b2d5908b4e3d93ec671dbed65e00ddcdfc274e9
Tom <tom.pillokat@googlemail.com>
2020-01-21 21:12:06 +0100

    Add nature data

---
+++ src/utils/natures.js
@@ -0,0 +1,136 @@
+const natures = () => [
+  'hardy',
+  'lonely',
+  'brave',
+  'adamant',
+  'naughty',
+  'bold',
+  'docile',
+  'relaxed',
+  'impish',
+  'lax',
+  'timid',
+  'hasty',
+  'serious',
+  'jolly',
+  'naive',
+  'modest',
+  'mild',
+  'quiet',
+  'bashful',
+  'rash',
+  'calm',
+  'gentle',
+  'sassy',
+  'careful',
+  'quirky'
+]
+
+// const naturesWithModifiers = () => {
+
+//  return{
+//    {
+//     inc: 'attack',
+//     dec: 'attack'
+//   },
+//   lonely: {
+//     inc: 'attack',
+//     dec: 'defense'
+//   },
+//   brave: {
+//     inc: 'attack',
+//     dec: 'speed'
+//   },
+//   adamant: {
+//     inc: 'attack',
+//     dec: 'special-attack'
+//   },
+//   naughty: {
+//     inc: 'attack',
+//     dec: 'special-defense'
+//   },
+//   bold: {
+//     inc: 'defense',
+//     dec: 'attack'
+//   },
+//   docile: {
+//     inc: 'defense',
+//     dec: 'defense'
+//   },
+//   relaxed: {
+//     inc: 'defense',
+//     dec: 'speed'
+//   },
+//   impish: {
+//     inc: 'defense',
+//     dec: 'special-attack'
+//   },
+//   lax: {
+//     inc: 'defense',
+//     dec: 'special-defense'
+//   },
+//   timid: {
+//     inc: 'speed',
+//     dec: 'attack'
+//   },
+//   hasty: {
+//     inc: 'speed',
+//     dec: 'defense'
+//   },
+//   serious: {
+//     inc: 'speed',
+//     dec: 'speed'
+//   },
+//   jolly: {
+//     inc: 'speed',
+//     dec: 'special-attack'
+//   },
+//   naive: {
+//     inc: 'speed',
+//     dec: 'special-defense'
+//   },
+//   modest: {
+//     inc: 'special-attack',
+//     dec: 'attack'
+//   },
+//   mild: {
+//     inc: 'special-attack',
+//     dec: 'defense'
+//   },
+//   quiet: {
+//     inc: 'special-attack',
+//     dec: 'speed'
+//   },
+//   bashful: {
+//     inc: 'special-attack',
+//     dec: 'special-attack'
+//   },
+//   rash: {
+//     inc: 'special-attack',
+//     dec: 'special-defense'
+//   },
+//   calm: {
+//     inc: 'special-defense',
+//     dec: 'attack'
+//   },
+//   gentle: {
+//     inc: 'special-defense',
+//     dec: 'defense'
+//   },
+//   sassy: {
+//     inc: 'special-defense',
+//     dec: 'speed'
+//   },
+//   careful: {
+//     inc: 'special-defense',
+//     dec: 'special-attack'
+//   },
+//   quirky: {
+//     inc: 'special-defense',
+//     dec: 'special-defense'
+//   },
+// }
+// }
+export default {
+  natures
+}
