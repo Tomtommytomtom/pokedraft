9a1f5cb82637133ac070588d0da8f0e8f90acf2b
Tom <tom.pillokat@googlemail.com>
2020-01-22 19:44:28 +0100

    Display Nature formatted

--- src/components/builder/selectors/NatureSelector.vue
+++ src/components/builder/selectors/NatureSelector.vue
@@ -1,5 +1,9 @@
 <template>
-  <v-select v-model="selectedNature" :items="natures" @change="emit"></v-select>
+  <v-select
+    v-model="selectedNatureWithInfo"
+    :items="selectedNatureArray"
+    @change="emit"
+  ></v-select>
 </template>
 
 <script>
@@ -38,22 +42,142 @@ export default {
         'sassy',
         'careful',
         'quirky'
-      ]
+      ],
+      natureInfo: {
+        hardy: {
+          inc: 'attack',
+          dec: 'attack'
+        },
+        lonely: {
+          inc: 'attack',
+          dec: 'defense'
+        },
+        brave: {
+          inc: 'attack',
+          dec: 'speed'
+        },
+        adamant: {
+          inc: 'attack',
+          dec: 'special-attack'
+        },
+        naughty: {
+          inc: 'attack',
+          dec: 'special-defense'
+        },
+        bold: {
+          inc: 'defense',
+          dec: 'attack'
+        },
+        docile: {
+          inc: 'defense',
+          dec: 'defense'
+        },
+        relaxed: {
+          inc: 'defense',
+          dec: 'speed'
+        },
+        impish: {
+          inc: 'defense',
+          dec: 'special-attack'
+        },
+        lax: {
+          inc: 'defense',
+          dec: 'special-defense'
+        },
+        timid: {
+          inc: 'speed',
+          dec: 'attack'
+        },
+        hasty: {
+          inc: 'speed',
+          dec: 'defense'
+        },
+        serious: {
+          inc: 'speed',
+          dec: 'speed'
+        },
+        jolly: {
+          inc: 'speed',
+          dec: 'special-attack'
+        },
+        naive: {
+          inc: 'speed',
+          dec: 'special-defense'
+        },
+        modest: {
+          inc: 'special-attack',
+          dec: 'attack'
+        },
+        mild: {
+          inc: 'special-attack',
+          dec: 'defense'
+        },
+        quiet: {
+          inc: 'special-attack',
+          dec: 'speed'
+        },
+        bashful: {
+          inc: 'special-attack',
+          dec: 'special-attack'
+        },
+        rash: {
+          inc: 'special-attack',
+          dec: 'special-defense'
+        },
+        calm: {
+          inc: 'special-defense',
+          dec: 'attack'
+        },
+        gentle: {
+          inc: 'special-defense',
+          dec: 'defense'
+        },
+        sassy: {
+          inc: 'special-defense',
+          dec: 'speed'
+        },
+        careful: {
+          inc: 'special-defense',
+          dec: 'special-attack'
+        },
+        quirky: {
+          inc: 'special-defense',
+          dec: 'special-defense'
+        }
+      }
     }
   },
   computed: {
-    natureDisplay: {
+    selectedNatureWithInfo: {
       get() {
-        return this.selectedNature.charAt(0).toUpperCase()
+        return this.format(this.selectedNature)
       },
       set(newValue) {
-        this.selectedNature = newValue.toLowerCase()
+        console.log(newValue.split(' ')[0].toLowerCase())
+        this.selectedNature = newValue.split(' ')[0].toLowerCase()
       }
+    },
+    selectedNatureArray() {
+      return this.natures.map(nature => this.format(nature))
+    }
+  },
+  watch: {
+    value() {
+      this.selectedNature = this.value
     }
   },
   methods: {
     emit() {
       this.$emit('input', this.selectedNature)
+    },
+    format(nature) {
+      const capNature = `${nature.charAt(0).toUpperCase()}${nature.substr(
+        1,
+        nature.length
+      )}`
+      const info = this.natureInfo[nature]
+      const infoString = `(+${info.inc}, -${info.dec})`
+      return `${capNature} ${infoString}`
     }
   }
 }
